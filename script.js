d962804b9ca5eed96a88999eae81c2fc8e3eaad2
ahmed <ahmedelmorabiti123460@gmail.com>
2026-04-29 20:37:23 +0100

    update

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ navbarToggle.addEventListener('click', () => {
     navbarToggle.classList.toggle('active');
     navbarMenu.classList.toggle('active');
 })
+
+const observerOptions = {
+    root: null,
+    threshold: 0.3
+};
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+}, observerOptions);
+
+const scrollElements = document.querySelectorAll('.section-container, .about-window, .introduction');
+
+scrollElements.forEach((el) => {
+    observer.observe(el);
+});
